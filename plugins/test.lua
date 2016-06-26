d0e48742a8daa812cbc1464e7d716b9fc09f24ad
SPARKTEA <rodealasade@gmail.com>
2016-06-26 04:35:15 +0300

    Delete test.lua

--- plugins/test.lua
+++
@@ -1,8 +0,0 @@
-local function run(msg)
-local text = [[*message* `for` _test_
-[Spheroch](http://telegram.me/spheroch)]]
-    send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
-end
-return {
- patterns = {"^!test$",},
- run = run }
